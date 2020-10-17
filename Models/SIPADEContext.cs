9009075c96b1751e7bc77930fdc02d0512134457
Yovel <yovellilo@gmial.com>
2020-10-16 23:41:05 -0600

    Prueba

--- Models/SIPADEContext.cs
+++ Models/SIPADEContext.cs
@@ -27,7 +27,7 @@ namespace SIPADE.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-ST2ET60;Database=SIPADE;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-NEQ7HN0;Database=SIPADE;Trusted_Connection=True;");
             }
         }
 
